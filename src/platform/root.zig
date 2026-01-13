15bbf0d4a6bdd374cc8dec8e335e0f40f8c0ab40
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2026-01-13 19:47:07 -0300

    feat(platform): add simple glfw window wrapper

--- src/platform/root.zig
+++ src/platform/root.zig
@@ -1,5 +1,7 @@
 const glfw = @import("glfw");
 
+pub const window = @import("window.zig");
+
 pub fn init() !void {
     try glfw.init();
 }
---
+++ src/platform/window.zig
@@ -0,0 +1,22 @@
+const glfw = @import("glfw");
+
+pub const Window = struct {
+    pointer: *glfw.Window,
+};
+
+pub fn create(width: c_int, height: c_int, title: [:0]const u8, monitor: ?*glfw.Monitor) !Window {
+    const window = try glfw.createWindow(width, height, title, monitor);
+    return .{ .pointer = window };
+}
+
+pub fn destroy(window: *Window) void {
+    window.pointer.shouldClose();
+}
+
+pub fn shouldClose(window: *Window) bool {
+    return window.pointer.shouldClose();
+}
+
+pub fn swapBuffers(window: *Window) void {
+    window.pointer.swapBuffers();
+}
