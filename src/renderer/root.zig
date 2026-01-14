2b67bf7d7f6fe02301ee4688ac1a46bca165e700
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2026-01-14 13:22:47 -0300

    feat(renderer): Integrate opengl backend

--- src/renderer/root.zig
+++ src/renderer/root.zig
@@ -1,3 +1,7 @@
+const opengl = @import("opengl");
+
+pub const BackendKind = enum { opengl };
+
 pub const Renderer = struct {
     ctx: *anyopaque,
     backend: *const Backend,
@@ -9,7 +13,46 @@ pub const Renderer = struct {
         endFrame: fn (*anyopaque) anyerror!void,
     };
 
-    pub fn create() !Renderer {}
+    pub fn create(backend: BackendKind) Renderer {
+        return switch (backend) {
+            .opengl => createOpenGLRenderer(),
+        };
+    }
+
+    fn createOpenGLRenderer() Renderer {
+        const gl = opengl.OpenGLRenderer{};
+        const backend = Backend{
+            .init = struct {
+                fn call(ctx: *anyopaque) anyerror!void {
+                    const self = @as(*opengl.OpenGLRenderer, @ptrCast(@alignCast(ctx)));
+                    try self.init();
+                }
+            }.call,
+            .deinit = struct {
+                fn call(ctx: *anyopaque) void {
+                    const self = @as(*opengl.OpenGLRenderer, @ptrCast(@alignCast(ctx)));
+                    self.deinit();
+                }
+            }.call,
+            .beginFrame = struct {
+                fn call(ctx: *anyopaque) anyerror!void {
+                    const self = @as(*opengl.OpenGLRenderer, @ptrCast(@alignCast(ctx)));
+                    try self.beginFrame();
+                }
+            }.call,
+            .endFrame = struct {
+                fn call(ctx: *anyopaque) anyerror!void {
+                    const self = @as(*opengl.OpenGLRenderer, @ptrCast(@alignCast(ctx)));
+                    try self.endFrame();
+                }
+            }.call,
+        };
+
+        return .{
+            .ctx = gl,
+            .backend = backend,
+        };
+    }
 
     pub fn init(self: *Renderer) anyerror!void {
         try self.backend.init(self.ctx);
