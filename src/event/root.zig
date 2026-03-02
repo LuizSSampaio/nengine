8e780366473322000c775689fee39ddbcc3d29a7
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2026-03-02 14:23:54 -0300

    feat(nengine/event): add handlers list for each event type

--- src/event/root.zig
+++ src/event/root.zig
@@ -6,22 +6,38 @@ pub const WindowEvent = event_mod.WindowEvent;
 pub const KeyEvent = event_mod.KeyEvent;
 pub const MouseEvent = event_mod.MouseEvent;
 
+pub const EventCallback = *const fn (event: *const Event) void;
+pub const EventTypes = blk: {
+    if (@typeInfo(Event) != .@"union") {
+        @compileError("Event must be an union");
+    }
+    break :blk std.meta.fields(Event).len;
+};
+
 pub var manager: ?EventManager = null;
 pub const EventManager = struct {
     pool_mutex: std.Thread.Mutex,
 
     pool: std.ArrayList(Event),
+    handlers: [EventTypes]std.ArrayList(EventCallback),
     allocator: std.mem.Allocator,
 
     pub fn init(allocator: std.mem.Allocator, buffer_size: usize) !void {
         var pool: std.ArrayList(Event) = .empty;
         errdefer pool.deinit(allocator);
 
+        var handlers: [EventTypes]std.ArrayList(EventCallback) = undefined;
+        for (&handlers) |*h| h.* = .empty;
+        errdefer {
+            for (&handlers) |*h| h.deinit(allocator);
+        }
+
         try pool.ensureTotalCapacity(allocator, buffer_size);
 
         manager = .{
             .pool_mutex = .{},
             .pool = pool,
+            .handlers = handlers,
             .allocator = allocator,
         };
     }
@@ -33,6 +49,7 @@ pub const EventManager = struct {
 
         var self = manager.?;
         self.pool.deinit(self.allocator);
+        for (&self.handlers) |*h| h.deinit(self.allocator);
         manager = null;
     }
 };
